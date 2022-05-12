a4a1ea75f0aec5dd042654a2775e934fbe350a8e
Nothingunknownn <68949238+Nothingunknownn@users.noreply.github.com>
2022-05-13 04:50:43 +0500

    a little CSS

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+^
+..idea/inspectionProfiles/profiles_settings.xml,1\e\1e9075f5bf079c01ef2c910709e91a497d262080
+E
+templates/__init__.py,1\6\16c0d3c42e61c674b2864f2d1afa1fd3f4cbaca1
--- flsite.py
+++ flsite.py
@@ -1,18 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'gfi2d235jgasdsdsds35jfdsfp12'
 
-menu = ['VK', 'Instagram', 'Telegram']
+menu = [{"name": "VK", "url": "vk-page"},
+        {"name": "Telegram", "url": "telegram-page"},
+        {"name": "Обратная связь", "url": "contact"}]
 
 
-@app.route('/')
+@app.route("/")
 def main():
     return render_template('main.html', menu=menu)
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
     return render_template('about.html', title='Обо мне', menu=menu)
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+
+    return render_template('contact.html', title='Обратная связь', menu=menu)
+
+
+# with app.test_request_context():
+#     print(url_for('main'))
+#     print(url_for('about'))
+#     print(url_for('profile', username="Kirill"))
 
 
 if __name__ == "__main__":
