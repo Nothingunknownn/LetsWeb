25b400b8f781c0c763c60b362f0c05c81e300690
Nothingunknownn <68949238+Nothingunknownn@users.noreply.github.com>
2022-05-13 21:31:13 +0500

    gradient + sometime templates

--- flsite.py
+++ flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'gfi2d235jgasdsdsds35jfdsfp12'
@@ -28,6 +28,33 @@ def contact():
 
     return render_template('contact.html', title='Обратная связь', menu=menu)
 
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "nothing" and request.form['psw'] == '123':
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
+    return f"Профиль пользователя: {username}"
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
+
+@app.errorhandler(401)
+def unauthorizedUser(error):
+    return render_template('page401.html', title="Пожалуйста, авторизуйтесь", menu=menu)
 
 # with app.test_request_context():
 #     print(url_for('main'))
