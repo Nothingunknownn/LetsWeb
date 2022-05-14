47a852a6a39cfec3db2624612846b94bc40a8d40
Nothingunknownn <68949238+Nothingunknownn@users.noreply.github.com>
2022-05-15 03:40:21 +0500

    + post page!

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,5 @@
 ..idea/inspectionProfiles/profiles_settings.xml,1\e\1e9075f5bf079c01ef2c910709e91a497d262080
 E
 templates/__init__.py,1\6\16c0d3c42e61c674b2864f2d1afa1fd3f4cbaca1
+F
+templates/page401.html,5\8\58288693feec7944903987a3d10886c7c163d18f
--- flsite.py
+++ flsite.py
@@ -1,21 +1,88 @@
-from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
+import sqlite3
+import os
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
+from FDataBase import FDataBase
+
+# конфигурация
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'gfi2d235jgasdsdsds35jfdsfp12'
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'gfi2d235jgasdsdsds35jfdsfp12'
+app.config.from_object(__name__)
 
-menu = [{"name": "VK", "url": "vk-page"},
-        {"name": "Telegram", "url": "telegram-page"},
-        {"name": "Обратная связь", "url": "contact"}]
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    """ Функция создающая таблицу """
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    """ Соединение с БД, если оно еще не установлено """
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.teardown_appcontext  # Срабатывает при уничтожении контекста приложения (момент завершения обработки запроса)
+def close_db(error):
+    """ Закрываем соединение с ДБ, если оно было установлено """
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
 
 
 @app.route("/")
 def main():
-    return render_template('main.html', menu=menu)
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('main.html', menu=dbase.getMenu(), posts=dbase.getPostsAnonce())
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'], request.form['url'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+
+    return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
+
+
+@app.route("/post/<alias>")
+def showPost(alias):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(alias)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='Обо мне', menu=menu)
+    return render_template('about.html', title='Обо мне', menu=[])
 
 
 @app.route("/contact", methods=["POST", "GET"])
@@ -26,7 +93,7 @@ def contact():
         else:
             flash('Ошибка отправки', category='error')
 
-    return render_template('contact.html', title='Обратная связь', menu=menu)
+    return render_template('contact.html', title='Обратная связь', menu=[])
 
 
 @app.route("/login", methods=["POST", "GET"])
@@ -37,7 +104,7 @@ def login():
         session['userLogged'] = request.form['username']
         return redirect(url_for('profile', username=session['userLogged']))
 
-    return render_template('login.html', title="Авторизация", menu=menu)
+    return render_template('login.html', title="Авторизация", menu=[])
 
 
 @app.route("/profile/<username>")
@@ -50,11 +117,12 @@ def profile(username):
 
 @app.errorhandler(404)
 def pageNotFound(error):
-    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
+    return render_template('page404.html', title="Страница не найдена", menu=[]), 404
+
 
 @app.errorhandler(401)
 def unauthorizedUser(error):
-    return render_template('page401.html', title="Пожалуйста, авторизуйтесь", menu=menu)
+    return render_template('page401.html', title="Пожалуйста, авторизуйтесь", menu=[]), 401
 
 # with app.test_request_context():
 #     print(url_for('main'))
